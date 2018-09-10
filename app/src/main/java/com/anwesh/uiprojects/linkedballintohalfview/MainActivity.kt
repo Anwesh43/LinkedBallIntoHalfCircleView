6c2f5e9361404fa4608bb635d29e1753af1ea4bb
Anwesh43 <anweshthecool0@gmail.com>
2018-09-10 12:06:41 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedballintohalfview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedballintohalfview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedballintohalfview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.ballintohalfview.BallIntoHalfView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BallIntoHalfView.create(this)
     }
 }
