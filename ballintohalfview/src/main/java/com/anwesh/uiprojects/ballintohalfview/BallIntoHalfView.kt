f01f5502f99e3e53217e7719f9ad66f05c6a5d39
Anwesh43 <anweshthecool0@gmail.com>
2018-09-10 11:50:38 +0530

    addin BIHNode class

--- ballintohalfview/src/main/java/com/anwesh/uiprojects/ballintohalfview/BallIntoHalfView.kt
+++ ballintohalfview/src/main/java/com/anwesh/uiprojects/ballintohalfview/BallIntoHalfView.kt
@@ -100,4 +100,48 @@ class BallIntoHalfView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BIHNode(var i : Int, val state : State = State()) {
+
+        private var prev : BIHNode? = null
+        private var next : BIHNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BIHNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBIHNode(i, state.scale, paint)
+            prev?.draw(canvas, paint)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BIHNode {
+            var curr : BIHNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
