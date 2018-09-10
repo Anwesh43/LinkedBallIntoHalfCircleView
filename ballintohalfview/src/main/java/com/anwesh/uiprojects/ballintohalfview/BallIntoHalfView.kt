70f896d7dd4e8394160e38eab1fb5002ff1109ae
Anwesh43 <anweshthecool0@gmail.com>
2018-09-10 11:33:13 +0530

    adding libraries and defining number of nodes

--- ballintohalfview/src/main/java/com/anwesh/uiprojects/ballintohalfview/BallIntoHalfView.kt
+++ ballintohalfview/src/main/java/com/anwesh/uiprojects/ballintohalfview/BallIntoHalfView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.ballintohalfview
 /**
  * Created by anweshmishra on 10/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
