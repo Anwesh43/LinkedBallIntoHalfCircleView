d23e651286e37f1394d25d529ca7e0733edc7263
Anwesh43 <anweshthecool0@gmail.com>
2018-09-10 12:14:16 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedballintohalfview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedballintohalfview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedballintohalfview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.ballintohalfview.BallIntoHalfView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         BallIntoHalfView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
